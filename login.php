061712ddc2bdd47f0fc0f1e2f3c2a64135d5f9e2
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-23 19:44:58 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+    include 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            Usuário: <input type="text" name="username"><br>
+            Senha: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
