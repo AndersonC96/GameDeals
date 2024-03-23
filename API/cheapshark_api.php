67f4cd2f89922de3c74b3f27b7101825674358f5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-23 20:00:03 -0300

    Update cheapshark_api.php

--- API/cheapshark_api.php
+++ API/cheapshark_api.php
@@ -1,6 +1,26 @@
 <?php
-    function buscarPromocoes(){
+    define('CHEAPSHARK_API_URL', 'https://www.cheapshark.com/api/1.0');
+    function buscarPromocoes($searchTerm = '', $storeFilter = '', $priceFilter = ''){
+        $deals = array();
+        $url = CHEAPSHARK_API_URL . "/deals?";
+        if(!empty($searchTerm)){
+            $url .= "title=" . urlencode($searchTerm) . "&";
+        }
+        if(!empty($storeFilter)){
+            $url .= "storeID=" . urlencode($storeFilter) . "&";
+        }
+        $response = file_get_contents($url);
+        if($response !== false){
+            $deals = json_decode($response, true);
+        }
+        return $deals;
     }
-    function buscarJogoEspecifico($nomeDoJogo){
+    function buscarDetalhesDoJogo($gameID){
+        $details = array();
+        $url = CHEAPSHARK_API_URL . "/games?id=" . urlencode($gameID);
+        $response = file_get_contents($url);
+        if($response !== false){
+            $details = json_decode($response, true);
+        }
+        return $details;
     }
-?>
