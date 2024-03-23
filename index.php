05b81c9cdfdbc9c5e3e6ad2de6868543c443afa6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-23 19:45:42 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include 'config.php';
+    include 'api/cheapshark_api.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Promoções de Jogos</title>
+    </head>
+    <body>
+    </body>
+</html>
